ca55e4d194591b44cbc45fb7bb547e50455d3ce4
Vitalii Pirko <vetalpirko@gmail.com>
2019-03-27 10:22:15 +0200

    change backup name

--- create-dump.sh
+++ create-dump.sh
@@ -3,4 +3,4 @@ set -o allexport
 [[ -f variables.env ]] && source variables.env
 set +o allexport
 
-docker exec ${APP_BASE_SLUG}_db_1 /usr/bin/mysqldump -u promptchat --password=$DB_PASSWORD promptchat  > ${APP_BASE_SLUG}-backup.sql
+docker exec ${APP_BASE_SLUG}_db_1 /usr/bin/mysqldump -u promptchat --password=$DB_PASSWORD promptchat  > backup.sql
