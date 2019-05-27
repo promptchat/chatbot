b173e9f4a6354b486e4db093f90b0ca279b922a5
Vitalii Pirko <vetalpirko@gmail.com>
2019-05-27 13:03:09 +0300

    fixes

--- docker_files/node/start.sh
+++ docker_files/node/start.sh
@@ -3,4 +3,9 @@
 yarn cache clean
 yarn install
 
+if [ -f /sources/laravel-echo-server.lock ]; then
+
+    ./node_modules/.bin/laravel-echo-server stop;
+fi
+
 ./node_modules/.bin/laravel-echo-server start --force
--- rund.sh
+++ rund.sh
@@ -3,4 +3,4 @@ set -o allexport
 [[ -f variables.env ]] && source variables.env
 set +o allexport
 
-docker-compose up
+docker-compose up --build
