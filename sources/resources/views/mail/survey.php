b8797be040d4cddfca74520b9bb1615f6820ca23
Vitalii Pirko <vetalpirko@gmail.com>
2019-09-12 12:40:46 +0300

    fix message send

--- sources/resources/views/mail/survey.php
+++ sources/resources/views/mail/survey.php
@@ -5,10 +5,12 @@
     <p><strong><?php echo app('translator')->getFromJson('mail.time_of_start'); ?>:</strong> <?php echo e($session->created_at); ?></p>
     <p><strong><?php echo app('translator')->getFromJson('mail.page'); ?>:</strong> <?php echo e($session->url); ?></p>
     <p><strong><?php echo app('translator')->getFromJson('mail.ip'); ?>:</strong> <?php echo e($session->ip); ?></p>
-
-    <p><?php echo app('translator')->getFromJson('mail.results'); ?>:</p>
-    <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <p><strong><?php echo e($key); ?>:</strong> <?php echo e($value); ?></p>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php if($data): ?>
+        <p><?php echo app('translator')->getFromJson('mail.results'); ?>:</p>
+        <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <p><strong><?php echo e($key); ?>:</strong> <?php echo e($value); ?></p>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('mail.mail', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
