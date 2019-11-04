bb715f5ce91bfe90765713a9554659c7cb4d7caf
Vitalii Pirko <vetalpirko@gmail.com>
2019-11-04 15:54:31 +0200

    fixes

--- load-dump.sh
+++ load-dump.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-source variables.env
-#source license.txt
-#export UID
+. ./env.sh
+
 
 cat backup.sql | docker-compose exec  -T db /usr/bin/mysql -u promptchat --password=$DB_PASSWORD promptchat
 
