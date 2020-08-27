4472fefc194930bfbe2fb2aeeeb2c4d6ed6e685f
Vitalii Pirko <vetalpirko@gmail.com>
2020-08-27 08:32:57 +0300

    Invites/Activity

--- sources/resources/views/mail/new-chatbot-activity.php
+++ sources/resources/views/mail/new-chatbot-activity.php
@@ -1,8 +1,12 @@
 <?php $__env->startSection('content'); ?>
 
     <p>New user is using chatbot!</p>
+    <div>
+        <?php echo $text; ?>
+
+    </div>
     <br/>
-    <a href="<?php echo e(action('AnalyticsController@showChatSessionStatistic', $seance->chat_session_id)); ?>"><?php echo app('translator')->getFromJson('mail.new_chatbot_activity'); ?></a>
+    <a href="<?php echo e(action('AnalyticsController@showChatSessionStatistic', $chatSession->id)); ?>"><?php echo app('translator')->getFromJson('mail.new_chatbot_activity'); ?></a>
 
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('mail.mail', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
