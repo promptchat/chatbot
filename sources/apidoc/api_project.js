427e447388ee20f2ea5072ff0b63ca5a90a825aa
Vitalii Pirko <vetalpirko@gmail.com>
2020-09-04 11:41:30 +0300

    doc

---
+++ sources/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "PROMPTCHAT",
+  "version": "1.0.0",
+  "description": "API",
+  "title": "PROMPTCHAT API",
+  "url": "/api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-09-04T08:32:54.464Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
