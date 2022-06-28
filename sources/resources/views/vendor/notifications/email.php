60f24c342ef44ae0bbe9802fa7835a4fa5771034
Alex <yakivchuk.sasha@gmail.com>
2022-06-28 12:27:37 +0300

    PROMPTCHAT-706 fix phone number validation

--- sources/resources/views/vendor/notifications/email.php
+++ sources/resources/views/vendor/notifications/email.php
@@ -33,10 +33,6 @@
 <?php $__env->startComponent('mail::button', ['url' => $actionUrl, 'color' => $color]); ?>
 <?php echo e($actionText); ?>
 
-<?php if (isset($__componentOriginalb8f5c8a6ad1b73985c32a4b97acff83989288b9e)): ?>
-<?php $component = $__componentOriginalb8f5c8a6ad1b73985c32a4b97acff83989288b9e; ?>
-<?php unset($__componentOriginalb8f5c8a6ad1b73985c32a4b97acff83989288b9e); ?>
-<?php endif; ?>
 <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 
@@ -68,9 +64,5 @@
     ]
 ); ?> <span class="break-all">[<?php echo e($displayableActionUrl); ?>](<?php echo e($actionUrl); ?>)</span>
 <?php $__env->endSlot(); ?>
-<?php endif; ?>
-<?php if (isset($__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d)): ?>
-<?php $component = $__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d; ?>
-<?php unset($__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d); ?>
 <?php endif; ?>
 <?php echo $__env->renderComponent(); ?>
