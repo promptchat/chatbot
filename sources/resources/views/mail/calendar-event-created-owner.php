fc454d45dd62cc726286fce25010c60963b526c5
Promptchat <promptchat@promptchat.com>
2022-10-14 09:37:03 +0200

    v2.13.1

--- sources/resources/views/mail/calendar-event-created-owner.php
+++ sources/resources/views/mail/calendar-event-created-owner.php
@@ -1,30 +1,30 @@
 <?php $__env->startSection('content'); ?>
-    <p>@translate('mail.website_visitor_requested_an_appointment_on') <a href="<?php echo e($host); ?>"><?php echo e($host); ?></a></p>
+    <p><?php echo t('mail.website_visitor_requested_an_appointment_on'); ?> <a href="<?php echo e($host); ?>"><?php echo e($host); ?></a></p>
 
     <p>
-        @translate('mail.details_the_visitor_left'):<br>
-        <strong>@translate('mail.name'):</strong> <?php echo e($event->username); ?><br>
-        <strong>@translate('mail.telephone'):</strong> <?php echo e($event->user_phone); ?><br>
-        <strong>@translate('mail.email'):</strong> <?php echo e($event->email); ?><br>
-        <strong>@translate('mail.comment'):</strong> <?php echo e($event->comment); ?><br>
-        <strong>@translate('mail.callback_requested_for'):</strong> <?php echo e((new \Carbon\Carbon($event->start_time))->format('Y-m-d H:i')); ?><br>
+        <?php echo t('mail.details_the_visitor_left'); ?>:<br>
+        <strong><?php echo t('mail.name'); ?>:</strong> <?php echo e($event->username); ?><br>
+        <strong><?php echo t('mail.telephone'); ?>:</strong> <?php echo e($event->user_phone); ?><br>
+        <strong><?php echo t('mail.email'); ?>:</strong> <?php echo e($event->email); ?><br>
+        <strong><?php echo t('mail.comment'); ?>:</strong> <?php echo e($event->comment); ?><br>
+        <strong><?php echo t('mail.callback_requested_for'); ?>:</strong> <?php echo e((new \Carbon\Carbon($event->start_time))->format('Y-m-d H:i')); ?><br>
     </p>
 
     <p>
-        @translate('mail.please_confirm_this_appointment_request')<br>
+        <?php echo t('mail.please_confirm_this_appointment_request'); ?><br>
         <a href="<?php echo e(env('APP_URL')."/calendars/".$event->calendar_id."/edit"); ?>">Click to log in to your <?php echo e(app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getTitle()); ?> calendar</a>
     </p>
 
     <div style="border: 2px solid #f4f4f4; border-radius: 10px; padding: 5px;">
-        <p>@translate('mail.additional_details_about_this_visitor'):</p>
+        <p><?php echo t('mail.additional_details_about_this_visitor'); ?>:</p>
         <p>
-            @translate('mail.chatbot_was_used_on_following_url'): <a href="<?php echo e($url); ?>"><?php echo e($url); ?></a><br>
-            @translate('mail.time_of_chat'): <?php echo e(Carbon\Carbon::now()); ?><br>
-            @translate('mail.visitor_ip'): <?php echo e($ip); ?><br>
+            <?php echo t('mail.chatbot_was_used_on_following_url'); ?>: <a href="<?php echo e($url); ?>"><?php echo e($url); ?></a><br>
+            <?php echo t('mail.time_of_chat'); ?>: <?php echo e(Carbon\Carbon::now()); ?><br>
+            <?php echo t('mail.visitor_ip'); ?>: <?php echo e($ip); ?><br>
         </p>
     </div>
 
-    <p>@translate('mail.with_kind_regards')</p>
+    <p><?php echo t('mail.with_kind_regards'); ?></p>
 
     <p><a href="<?php echo e(url('/')); ?>"><?php echo e(app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getTitle()); ?></a></p>
 <?php $__env->stopSection(); ?>
--- sources/resources/views/mail/calendar-event-created.php
+++ sources/resources/views/mail/calendar-event-created.php
@@ -1,22 +1,22 @@
 <?php $__env->startSection('content'); ?>
-    <p>@translate('mail.thank_you_for_your_request_for_appointment_on') <a href="<?php echo e($host); ?>"><?php echo e($host); ?></a></p>
+    <p><?php echo t('mail.thank_you_for_your_request_for_appointment_on'); ?> <a href="<?php echo e($host); ?>"><?php echo e($host); ?></a></p>
 
     <p>
-        @translate('mail.the_details_you_left_with_us'):<br>
-        <strong>@translate('mail.your_name'):</strong> <?php echo e($event->username); ?><br>
-        <strong>@translate('mail.telephone'):</strong> <?php echo e($event->user_phone); ?><br>
-        <strong>@translate('mail.email'):</strong> <?php echo e($event->email); ?><br>
-        <strong>@translate('mail.comment'):</strong> <?php echo e($event->comment); ?><br>
-        <strong>@translate('mail.callback_requested_for'):</strong> <?php echo e((new \Carbon\Carbon($event->start_time))->format('Y-m-d H:i')); ?><br>
+        <?php echo t('mail.the_details_you_left_with_us'); ?>:<br>
+        <strong><?php echo t('mail.your_name'); ?>:</strong> <?php echo e($event->username); ?><br>
+        <strong><?php echo t('mail.telephone'); ?>:</strong> <?php echo e($event->user_phone); ?><br>
+        <strong><?php echo t('mail.email'); ?>:</strong> <?php echo e($event->email); ?><br>
+        <strong><?php echo t('mail.comment'); ?>:</strong> <?php echo e($event->comment); ?><br>
+        <strong><?php echo t('mail.callback_requested_for'); ?>:</strong> <?php echo e((new \Carbon\Carbon($event->start_time))->format('Y-m-d H:i')); ?><br>
     </p>
 
-    <p>@translate('mail.please_note_your_appointment_request')</p>
+    <p><?php echo t('mail.please_note_your_appointment_request'); ?></p>
 
-    <p>@translate('mail.we_aim_to_confirm_all_appointment_requests')</p>
+    <p><?php echo t('mail.we_aim_to_confirm_all_appointment_requests'); ?></p>
 
-    <p>@translate('mail.please_bear_with_us_until_one')</p>
+    <p><?php echo t('mail.please_bear_with_us_until_one'); ?></p>
 
-    <p>@translate('mail.with_kind_regards')</p>
+    <p><?php echo t('mail.with_kind_regards'); ?></p>
 
     <p><a href="<?php echo e(url('/')); ?>"><?php echo e(app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getTitle()); ?></a></p>
 <?php $__env->stopSection(); ?>
