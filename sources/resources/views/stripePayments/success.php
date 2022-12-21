fccf3f8e3e9a4d59ba6e5888cea6ef400d3008d5
Promptchat <promptchat@promptchat.com>
2022-12-21 15:50:56 +0100

    v2.16.4

---
+++ sources/resources/views/stripePayments/success.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('body'); ?>
+    <div id="app">
+        Payment success
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
