610c26abf77db2915a1e6cb9cdec98904a24296e
Promptchat <promptchat@promptchat.com>
2023-04-19 10:36:41 +0200

    v3.0.0

--- sources/resources/views/app.php
+++ sources/resources/views/app.php
@@ -2,12 +2,16 @@
     <script>
         window.siteConfigs = {
             primaryColor: "<?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getColorScheme()['primary']; ?>",
+            sideBar: <?php echo json_encode(app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getSidebarAppearance()); ?>,
+            loginPage: "<?php echo \Config::get('auth.isExternal') ? app()->make(\App\Services\ExternalUserResolveService::class)->getLoginPage() : ""; ?>",
+            logoutPage: "<?php echo \Config::get('auth.isExternal') ? app()->make(\App\Services\ExternalUserResolveService::class)->getLogoutPage() : "/logout"; ?>",
         }
     </script>
     <style id="color-variables">
         :root {
             --primary-color: <?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getColorScheme()['primary']; ?>;
             --secondary-color: <?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getColorScheme()['secondary']; ?>;
+            --background-color: <?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getColorScheme()['background']; ?>;
         }
     </style>
     <style id="menu-panel-variables">
@@ -17,7 +21,12 @@
     </style>
     <style id="chatboard-variables">
         :root {
-            --chatboard-background: <?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getChatboardBackground(); ?>;
+            --chatboard-background: <?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getColorScheme()['chatboard']; ?>;
+        }
+    </style>
+    <style >
+        .auth-wallpaper {
+            background: url(<?php echo app(\App\Repositories\Interfaces\ConfigsRepositoryInterface::class)->getAuthBackground(); ?>) no-repeat;
         }
     </style>
 <?php $__env->stopPush(); ?>
