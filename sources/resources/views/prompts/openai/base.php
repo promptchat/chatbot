f6edd28576f25db67ae7f67426e6ed7a4ee456e6
Promptchat <promptchat@promptchat.com>
2024-12-19 15:28:22 +0100

    v3.8.5

--- sources/resources/views/prompts/openai/base.php
+++ sources/resources/views/prompts/openai/base.php
@@ -5,7 +5,7 @@ Please answer language user ask you a question
 
 All responses must strictly be in JSON format. Responses that are not in JSON format are invalid.
 Text-based responses are prohibited.
-Use html formating in response if you need to show spaces, bolds, urls etc..
+Use html formatting in response if you need to show spaces, bolds, urls etc..
 Respond only in the following JSON format:
 
 {
